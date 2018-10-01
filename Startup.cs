febcd90797dbf008122db3e72d31db118dfff24d
Jeferson da Silva Araujo <jeff.araujo@gmail.com>
2018-10-01 19:40:55 -0300

    Realizando as chamadas dos Middlewares (Classe Startup / Método Configure)

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Fiap01.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -18,33 +19,57 @@ namespace Fiap01
         }
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-                app.UseDeveloperExceptionPage();
-
-            app.UseStaticFiles();
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                 name: "default",
-                 template: "{controller=Home}/{action=Index}/{id?}");
-
-                //routes.MapRoute(
-                //   name: "autor",
-                //   template: "autor/{nome}",
-                //   defaults: new { controller = "Autor", action = "Index" });
-
-                //routes.MapRoute(
-                //        name: "topicosdacategoria",
-                //        template: "{categoria}/{topico}",
-                //        defaults: new { controller = "Topicos", action = "Index" });
-
-                //routes.MapRoute(
-                //   name: "autoresDoAno",
-                //   template: "{ano:int}/autor",
-                //   defaults: new { controller = "Autor", action = "ListaDosAutoresDoAno" });
+            
+            //Realizando a chamada de Middleware
+            //Middleware 1...
+            app.Use((context, next) => {
+                context.Response.Headers.Add("X-Teste", "headerteste");
+                return next();
 
             });
+            //Chamando Middleware 2...
+            app.Use(async (context, next) =>
+            {
+                var teste = 123;
+                await next.Invoke();
+                var teste2 = 1234;
+            });
+
+            //Chamando Middleware 3...
+            app.Run(async context => {
+                await context.Response.WriteAsync("boa noite");
+            });
+
+
+
+
+            //if (env.IsDevelopment())
+            //    app.UseDeveloperExceptionPage();
+
+            //app.UseStaticFiles();
+
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute(
+            //     name: "default",
+            //     template: "{controller=Home}/{action=Index}/{id?}");
+
+            //    //routes.MapRoute(
+            //    //   name: "autor",
+            //    //   template: "autor/{nome}",
+            //    //   defaults: new { controller = "Autor", action = "Index" });
+
+            //    //routes.MapRoute(
+            //    //        name: "topicosdacategoria",
+            //    //        template: "{categoria}/{topico}",
+            //    //        defaults: new { controller = "Topicos", action = "Index" });
+
+            //    //routes.MapRoute(
+            //    //   name: "autoresDoAno",
+            //    //   template: "{ano:int}/autor",
+            //    //   defaults: new { controller = "Autor", action = "ListaDosAutoresDoAno" });
+
+            //});
         }
     }
 }
